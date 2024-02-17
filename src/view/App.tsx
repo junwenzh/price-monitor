539a3f884af5d11f312118343fc6b952666407df
Julia Xin <julia.zl.xin@gmail.com>
2024-02-17 13:43:45 -0500

    Set up react

---
+++ src/view/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <iframe width="800" height="600" src="https://www.ucsb.edu/"></iframe>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/view/index.tsx'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  mode: process.env.NODE_ENV,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          target: 'es2015',
+        },
+      },
+      {
+        test: /.s?[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/view/index.html',
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    proxy: [{
+      '/api': 'http://localhost:3000',
+    }]
+  },
+};
