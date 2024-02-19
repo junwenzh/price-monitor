5b58fd9c165392cfff2a896efac35150f687577b
Jun <junwenzhang@outlook.com>
2024-02-18 21:21:31 -0500

    Added unit test for the fetch endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
+import { fetch } from '@/utils/fetch';
+import { test } from '@/utils/scraper';
+import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
-import express, { Request, Response, NextFunction } from 'express';
-import { test } from './utils/scraper';
-import { fetch } from './utils/fetch';
 
 const app = express();
 const port = 8084;
@@ -39,6 +39,8 @@ app.get('/api/*', (req: Request, res: Response) => {
   res.status(404).json({ message: 'API route not found' });
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
+
+export default server;
