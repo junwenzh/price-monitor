421d455fce1dda2b796f2804deea7153a3176f3f
Jun <junwenzhang@outlook.com>
2024-02-24 22:55:08 -0500

    Cleaned up the scrape function. Added styling

--- src/utils/playwright.ts
+++ src/utils/playwright.ts
@@ -14,64 +14,68 @@ async function scrape(url: string) {
   await page.waitForLoadState('domcontentloaded'); // this doesn't work on its own
 
   await page.evaluate(() => {
-    document.querySelectorAll('script').forEach(script => script.remove());
+    // remove scripts
+    const scriptEles = document.querySelectorAll('script');
+    scriptEles.forEach(script => script.remove());
 
-    document
-      .querySelectorAll(
-        '[href^="javascript:"], [src^="javascript:"], [action^="javascript:"]'
-      )
-      .forEach(el => {
-        el.removeAttribute('href');
-        el.removeAttribute('src');
-        el.removeAttribute('action');
-      });
-
-    // Example of removing event handlers - this would need to be done for all possible event attributes
-    document.querySelectorAll('[onclick], [onerror], [onload]').forEach(el => {
-      el.removeAttribute('onclick');
-      el.removeAttribute('onerror');
-      el.removeAttribute('onload');
+    const javascriptEles = document.querySelectorAll(
+      '[href^="javascript:"], [src^="javascript:"], [action^="javascript:"]'
+    );
+    javascriptEles.forEach(el => {
+      el.removeAttribute('href');
+      el.removeAttribute('src');
+      el.removeAttribute('action');
     });
 
-    const aTags = document.querySelectorAll('a');
+    const activeEles = document.querySelectorAll(
+      '[onclick], [onload], [onerror]'
+    );
+    activeEles.forEach(el => {
+      el.removeAttribute('onclick');
+      el.removeAttribute('onload');
+      el.removeAttribute('onerror');
+    });
 
-    aTags?.forEach(tag => {
+    // remove links
+    const aTags = document.querySelectorAll('a');
+    aTags.forEach(tag => {
       tag.addEventListener('click', event => {
         event.preventDefault();
       });
       tag.removeAttribute('href');
     });
 
-    const allEles = document.querySelectorAll('*');
-    const allElesArray = Array.from(allEles!);
-    const textEles = allElesArray.filter(el => {
-      const children = el.childNodes; // direct children(
-      const childrenArray = Array.from(children);
-      const hasTextChild = childrenArray.some(node => {
-        if (node.nodeType === 3 && node.textContent?.trim() !== '') {
-          return true;
-        }
-        return false;
-      });
-      return hasTextChild;
-    });
-
-    textEles?.forEach(ele => {
-      // if (ele.childElementCount === 0) {
-      ele.classList.add('hover:border-2', 'hover:border-orange-300');
-      // }
-    });
-
-    // not display error images
+    // hide errored images
     const imgEles = document.querySelectorAll('img');
-    imgEles?.forEach(el => {
+    imgEles.forEach(el => {
       el.setAttribute('onerror', 'this.style.diplay="none"');
+    });
+
+    // disable events
+    const eventTypes = ['click', 'submit', 'change', 'input', 'form'];
+
+    eventTypes.forEach(type => {
+      document.addEventListener(
+        type,
+        function (event) {
+          event.stopPropagation();
+          event.preventDefault();
+        },
+        true
+      );
+    });
+
+    // disable all form submits
+    const formEles = document.querySelectorAll('form');
+    formEles.forEach(form => {
+      form.onsubmit = null;
+      form.addEventListener('submit', function (event) {
+        event.preventDefault();
+      });
     });
   });
 
   const content = await page.content();
-
-  // console.log(typeof content, 'content type');
 
   await browser.close();
   return content;
