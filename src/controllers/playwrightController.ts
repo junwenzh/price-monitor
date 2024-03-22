9512b10757a95d73a7c41100ae8a8e95387de728
Julia Xin <julia.zl.xin@gmail.com>
2024-03-21 20:40:24 -0400

    moved scrape functions to controller

--- src/controllers/playwrightController.ts
+++ src/controllers/playwrightController.ts
@@ -1,15 +1,59 @@
-import { playwrightConnection } from '@/utils/playwright';
-import { Request, Response } from 'express';
+import { playwrightConnection, getPriceElements } from '@/utils/playwright';
+import { Request, Response, NextFunction } from 'express';
 
 class PlaywrightController {
-  async getScreenshot(req: Request, res: Response) {
-    const url = res.locals.url as string;
+  // original scrape
+  async getScreenshot(req: Request, res: Response, next: NextFunction) {
+    const url = req.body.url as string;
     if (!url) {
+      return next({
+        message: {
+          err: 'Missing URL',
+        },
+      });
     }
 
     const page = await playwrightConnection.getPage(url);
+    const screenshotBuffer = await page.screenshot({ fullPage: false });
+    const screenshotString = screenshotBuffer.toString('base64');
+    res.locals.screenshot = screenshotString;
+    return next();
   }
-  async getElementAtXY(req: Request, res: Response) {}
+
+  async getElementAtXY(req: Request, res: Response, next: NextFunction) {
+    const url = req.body.url as string;
+    const coordinates = req.body.coordinates as { x: number; y: number };
+
+    // console.log(url, coordinates);
+
+    if (!url || !coordinates) {
+      return next({
+        message: {
+          err: 'Missing URL or coordinates',
+        },
+      });
+    }
+
+    const page = await playwrightConnection.getPage(url);
+
+    // console.log('page', page);
+
+    const elements = await page.evaluate(getPriceElements, coordinates);
+
+    // console.log('elements', elements);
+
+    if (elements.price !== 'Not found') {
+      res.locals.price = elements.price;
+      res.locals.selector = elements.selector;
+      return next();
+    } else {
+      return next({
+        message: {
+          err: 'Price not found',
+        },
+      });
+    }
+  }
 }
 
 export default new PlaywrightController();
--- src/routes/playwrightRouter.ts
+++ src/routes/playwrightRouter.ts
@@ -1,38 +1,23 @@
-import { scrape, scrape2 } from '@/utils/playwright';
+//import { scrape, scrape2 } from '@/utils/playwright';
 import express, { Request, Response } from 'express';
+import playwrightController from '@/controllers/playwrightController';
 
 const router = express.Router();
 
-router.post('/screenshot', async (req: Request, res: Response) => {
-  // const url = req.query.url as string | undefined;
-  const url = req.body.url as string;
-
-  console.log('get', url);
-
-  if (!url) {
-    res.json({ message: 'No url provided' });
+router.post(
+  '/screenshot',
+  playwrightController.getScreenshot,
+  (req: Request, res: Response) => {
+    return res.json({ screenshot: res.locals.screenshot });
   }
+);
 
-  const response = await scrape(url);
-
-  res.send(response);
-});
-
-router.post('/coordinates', async (req: Request, res: Response) => {
-  // const { url, x, y } = req.body;
-  const url = req.body.url as string;
-  const x = req.body.x as string;
-  const y = req.body.y as string;
-
-  console.log('post', url);
-
-  if (!url || !x || !y) {
-    return res.json({ message: 'Missing argument' });
+router.post(
+  '/coordinates',
+  playwrightController.getElementAtXY,
+  (req: Request, res: Response) => {
+    return res.json({ price: res.locals.price, selector: res.locals.selector });
   }
-
-  const element = await scrape2({ url, x: Number(x), y: Number(y) });
-
-  return res.json(element);
-});
+);
 
 export default router;
