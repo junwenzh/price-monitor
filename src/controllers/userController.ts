272719a7de0a2e4808f7d1e1c92c892ae85096aa
Jun <junwenzhang@outlook.com>
2024-04-13 14:46:01 -0400

    Added authenticateJWT to the userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,14 @@
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 //import { db, DB } from '@/database/db';
 import { userDb } from '@/database/userdb';
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
+
+interface AuthenticatedRequest extends Request {
+  username?: string;
+}
 
 const userController = {
   async createUser(req: Request, res: Response, next: NextFunction) {
@@ -51,6 +55,35 @@ const userController = {
       res.json({ token, username: user.username, email: user.email });
     } catch (error) {
       next(error);
+    }
+  },
+
+  async authenticateJWT(
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+  ) {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return next({
+        status: 401,
+        message: 'Authentication failed: No token provided',
+      });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || '') as {
+        username: string;
+      };
+      req.username = decoded.username;
+      console.log(decoded);
+      next();
+    } catch (error) {
+      return next({
+        status: 403,
+        message: 'Authentication failed: Invalid token',
+      });
     }
   },
 };
